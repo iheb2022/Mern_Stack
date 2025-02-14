3f6848554dee78eede01fca290e26333792c894b
iheb cherni <eheebhob@gmail.com>
2025-02-14 02:58:36 +0100

    Mern_stack

---
+++ Mern/Week_1/Day_1/Core/First React with JSX/first-react/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Dojo</h1>
+      <ul>
+        <li>learn React</li>
+        <li>Climb Mt Everrest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
